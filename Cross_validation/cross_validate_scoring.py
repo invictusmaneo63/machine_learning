65a56146ee6804c02b7495052724f558855cf891
Prem Kumar Tiwari <31136562+invictusmaneo63@users.noreply.github.com>
2020-01-08 23:43:10 +0100

    Add files via upload

---
+++ Cross_validation/cross_validate_scoring.py
@@ -0,0 +1,9 @@
+from sklearn import svm, datasets
+from sklearn.model_selection import cross_val_score
+
+X, y = datasets.load_iris(return_X_y= True)
+clf = svm.SVC(random_state=0, gamma='scale')
+macro= cross_val_score(clf, X, y, cv=5, scoring= 'recall_macro')
+f1 = cross_val_score(clf, X, y, cv=5, scoring= 'f1_macro')
+print(f1)
+print(macro)
