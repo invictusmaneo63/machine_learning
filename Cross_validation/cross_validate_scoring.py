750725ca47044d1597e3e048fd4cf8934fa2a327
Prem Kumar Tiwari <31136562+invictusmaneo63@users.noreply.github.com>
2020-01-08 23:51:43 +0100

    Update cross_validate_scoring.py

--- Cross_validation/cross_validate_scoring.py
+++ Cross_validation/cross_validate_scoring.py
@@ -7,3 +7,7 @@ macro= cross_val_score(clf, X, y, cv=5, scoring= 'recall_macro')
 f1 = cross_val_score(clf, X, y, cv=5, scoring= 'f1_macro')
 print(f1)
 print(macro)
+
+# to check th evarious possible scoring methods applied for cross_val_score
+z = sorted(sklearn.metrics.SCORERS.keys())
+print(z)
